dd7c2b4b49de7c0d1e89a19ae19e826052c39f55
ChuckTirtasaputra <101743291+ChuckTirtasaputra@users.noreply.github.com>
2024-02-15 11:31:46 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
+/// 2 alerts a day
+/// 1st 9am-3pm
+/// 2nd from 4pm-10pm
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Link } from 'expo-router';
 import { StyleSheet, Text, View, Button, Alert, Pressable } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text
-      style = {styles.logo}>BeFeel!</Text>
-      <Pressable style = {styles.login}
-      onPress={() => alert('LOGIN')}>Login</Pressable>
+      <Text style = {styles.logo}>BeFeel!</Text>
+      <Pressable onPress={() => Alert.alert('LOGIN')}><Text style = {styles.login}>Login</Text></Pressable>
+      <Pressable onPress={() => Alert.alert('ALERT USER')}><Text style = {styles.login}>Alert</Text></Pressable>
       <Button 
       title="Login"
-      style = {styles.login}
-      onPress={() => Alert.alert('LOGIN')}
+      onPress={() => <Link href="sign-in.js"><Text>login</Text></Link>}
       ></Button>
+
+      <Link href="sign-in.js" asChild>
+      <Pressable>
+        <Text>Home</Text>
+      </Pressable>
+      </Link>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -24,21 +35,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 8,
     paddingHorizontal: 35,
+    marginVertical: 30,
     borderRadius: 4,
-    fontSize: "1.52em",
     elevation: 3,
     backgroundColor: '#191970',
     color: "#fff",
   },
   logo: {
-    fontSize: "2.52rem",
+    fontSize: 50,
     fontWeight: 'bold',
     letterSpacing: 0.25,
-    color: '#000',
+    color: '#fff',
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     padding: 100
   },
