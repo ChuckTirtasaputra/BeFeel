fdea3726041f3532d7b27220c8932685442fd898
ChuckTirtasaputra <101743291+ChuckTirtasaputra@users.noreply.github.com>
2024-02-20 18:31:22 -0500

    Add files via upload

---
+++ app/login.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const login = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+export default login
+
+const styles = StyleSheet.create({})
