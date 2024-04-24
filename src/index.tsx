923833cd8ade0cfbda09972dbff9317e08c1e1ca
marcospg10 <70762206+marcospg10@users.noreply.github.com>
2024-04-24 09:01:49 -0400

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { db } from '../config';
+import { ref, set } from 'firebase/database';
+
+const AddData: React.FC = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    //function to add data to firebase realtime db
+    const dataAddOn = () => {
+        set(ref(db, 'posts/' + title), {
+            title: title,
+            body: body,
+        });
+        setTitle('');
+        setBody('');
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Post Your Mood</Text>
+            <TextInput
+                placeholder='Title'
+                value={title}
+                onChangeText={(text) => setTitle(text)}
+                style={styles.input}
+            />
+            <TextInput
+                placeholder='Body'
+                value={body}
+                onChangeText={(text) => setBody(text)}
+                style={styles.input}
+            />
+            <Button title='Add Data' onPress={dataAddOn} />
+        </View>
+    );
+};
+
+export default AddData;
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 100,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'black',
+        margin: 10,
+        padding: 10,
+        fontSize: 18,
+        borderRadius: 6,
+    },
+});
