f6beeee46d718f67e2fb31588a9afe894751fb5b
ChuckTirtasaputra <101743291+ChuckTirtasaputra@users.noreply.github.com>
2024-04-25 15:02:01 -0400

    Update Profile.tsx

--- app/Profile.tsx
+++ app/Profile.tsx
@@ -1,8 +1,7 @@
-import { Link } from 'expo-router'
 import { StyleSheet, Text, View, Pressable, ScrollView } from 'react-native';
 import React from 'react';
 import { NavigationProp } from '@react-navigation/native'
-import { FIREBASE_AUTH } from '../FirebaseConfig';
+import Userinfo from '../src/Userinfo';
 
 interface RouterProps {
   navigation: NavigationProp<any, any>
@@ -10,11 +9,8 @@ interface RouterProps {
 
 const Profile = ({ navigation }: RouterProps) => {
     return (
-        <View>
-        <Link  href="/Login" 
-        style={styles.links}>
-            Login
-        </Link>
+        <View style={styles.container}>
+          <Userinfo />
         </View>
     )
 }
@@ -23,30 +19,8 @@ export default Profile
 
 
 const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: 'pink',
-      },
-      post: {
-        fontSize: 25,
-        fontWeight: 'normal',
-        color: '#000',
-        marginHorizontal: 20,
-      },
-      links: {
-        fontSize: 20,
-        fontWeight: '700',
-        color: '#23679e',
-        padding: 10,
-      },
       container: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: "space-around"
-      },
-      separators: {
-        height: 3,
-        backgroundColor: '#000', 
-        marginHorizontal: 10,
-        marginVertical: 5.
+        flex: 1,
+        backgroundColor: 'black',
       },
 })
